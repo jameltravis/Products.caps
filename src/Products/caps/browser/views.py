e0cc52dcd403ec547d410d7069a80ca13f87cd9d
vagrant <vagrant@vagrant.vm>
2018-01-24 16:53:46 +0000

    completed index page view

--- src/Products/caps/browser/views.py
+++ src/Products/caps/browser/views.py
@@ -5,81 +5,26 @@ from Products.Five.browser import BrowserView
 from plone import api
 
 
+# get catalog tool to query DB
+CATALOG = api.portal.get_tool('portal_catalog')
+
+# Actually Querying the DB
+READMISSIONS = CATALOG.searchResults(**{'portal_type': 'Readmission'})
+EXTRA_CREDS = CATALOG.searchResults(**{'portal_type': 'ExtraCredits'})
+GRADE_CHANGE = CATALOG.searchResults(**{'portal_type': 'GradeChange'})
+LEAVE_ABS = CATALOG.searchResults(**{'portal_type': 'LeaveAbs'})
+
 class DashView(BrowserView):
     """
     Classs for finding lists of a given petition type.
     """
 
-    def readmission_petitions(self):
-        """Readmission petitions"""
-
-        results = []
-        # catalog = api.portal.get_tool('portal_catalog')
-        brains = api.content.find(portal_type='Readmission')
-
-        for brain in brains:
-            results.append({
-                'title': brain.Title,
-                'emplID': brain.emplID,
-                'review_state': brain.review_state
-            })
-        return sorted(results, key=itemgetter('review_state'))
-
-    def extra_credit_petitions(self):
-        """Petitions for extra credits"""
-
-        results = []
-        # catalog = api.portal.get_tool('portal_catalog')
-        brains = api.content.find(portal_type='ExtraCredits')
-
-        for brain in brains:
-            results.append({
-                'title': brain.Title,
-                'emplID': brain.emplID,
-                'review_state': brain.review_state
-            })
-        return sorted(results, key=itemgetter('review_state'))
-
-    def leave_abs_petitions(self):
-        """Petitions for Leave of Absence and Retroactive Withdrawals"""
-
-        results = []
-        # catalog = api.portal.get_tool('portal_catalog')
-        brains = api.content.find(portal_type='LeaveAbs')
-
-        for brain in brains:
-            results.append({
-                'title': brain.Title,
-                'emplID': brain.emplID,
-                'review_state': brain.review_state
-            })
-        return sorted(results, key=itemgetter('review_state'))
-
-    def grade_appeal_petitions(self):
-        """Petitions to have a single grade appealed"""
-
-        results = []
-        # catalog = api.portal.get_tool('portal_catalog')
-        brains = api.content.find(portal_type='GradeAppeal')
-
-        for brain in brains:
-            results.append({
-                'title': brain.Title,
-                'emplID': brain.emplID,
-                'CreationDate': brain.CreationDate,
-                'review_state': brain.review_state
-            })
-        return sorted(results, key=itemgetter('review_state'))
-
     def find_readmissions(self):
-        """Use portal_catalog this time to find readmission petitions"""
-        # get catalog tool to query DB
-        catalog = api.portal.get_tool('portal_catalog')
-        # Actually Querying the DB
-        results = catalog.searchResults(**{'portal_type': 'Readmission'})
+        """Use portal_catalog this time to find readmission petitions."""
+        
         petitions = []
         # Put the query results in a list and get attributes of interest
-        for brains in results:
+        for brains in READMISSIONS:
             petitions.append({
                 'title': brains.Title,
                 'emplID': brains.emplID,
@@ -89,5 +34,27 @@ class DashView(BrowserView):
         return sorted(petitions, key=itemgetter('review_state'))
 
 class IndexView(BrowserView):
-    """Renders caps index template"""
-    pass
+    """Methods for the index page of CAPS."""
+
+    def pending_readmissions(self):
+        """returns a list of readmission objects with 'private' review_states."""
+
+        return filter(lambda READMISSIONS: READMISSIONS['review_state'] == 'private', READMISSIONS)
+
+
+    def pending_extra_credits(self):
+        """returns a list of readmission objects with 'private' review_states."""
+
+        return filter(lambda EXTRA_CREDS: EXTRA_CREDS['review_state'] == 'private', EXTRA_CREDS)
+
+
+    def pending_grade_changes(self):
+        """returns a list of readmission objects with 'private' review_states."""
+
+        return filter(lambda GRADE_CHANGE: GRADE_CHANGE['review_state'] == 'private', GRADE_CHANGE)
+
+
+    def pending_leave_abs(self):
+        """returns a list of readmission objects with 'private' review_states."""
+
+        return filter(lambda LEAVE_ABS: LEAVE_ABS['review_state'] == 'private', LEAVE_ABS)
